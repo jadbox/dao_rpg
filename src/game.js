246d26bc97df0294dc29a24381d3940d5663e744
Jonathan Dunlap <jdunlap@outlook.com>
2020-04-24 16:51:33 -0700

    PoC

---
+++ src/game.js
@@ -0,0 +1,139 @@
+const MOBS = ["rat", "skeleton", "slug"];
+const MOBS_STATS = { rat: {hp: 1, name: 'rat', dm: 1}, skeleton: {hp:1, name: 'skeleton', dm: 1}, slug: {hp:1, name: 'slug', dm: 1}};
+
+function State(name) {
+    return {
+        name: name,
+        mobs: [],
+        ticks: 0
+    }
+}
+class Game {
+    constructor(room) {
+        console.log('Game room', room);
+        this.room = room || { players: {'jadbox':{name:'jadbox', hp: 20, dm: 3 }} };
+        this.players = this.room.players;
+        this.playersArr = Object.keys(this.room.players);
+        this.playersLen = this.playersArr.length;
+        this.state = State('start');
+    }
+    // onState (event) => ()
+    start(onState) {
+        if(this.loop) throw new Error('already started');
+
+        console.log('starting game');
+        this.onState = onState;
+
+        const SPEED = 3500;
+        this.loop = setInterval(this._run.bind(this), SPEED);
+        this.started = true;
+    }
+    act(player, cmd) {
+        let action = null;
+        let p = this.players[player];
+
+        switch(cmd) {
+            case "attack": {
+                if(!this.state.mobs?.length) {
+                    action =`${player} can't find anything here to attack.`;
+                    break;
+                }
+                const mobIx = rint(this.state.mobs.length);
+                const mob = this.state.mobs[mobIx];
+                const dm = rint(p.dm, 1);
+
+                action =`${player} attacks ${mob.name} for ${dm}!`;
+
+                mob.hp -= dm;
+                if(mob.hp>0) action += ` ${mob.name} has ${mob.hp}hp left.`;
+                else {
+                    action += ` ${mob.name} has died!`;
+                    this.state.mobs = this.state.mobs.filter(x=>x.hp > 0);
+                }
+            }
+        }
+
+
+        if(action && this.onState) this.onState(action);
+    }
+    _genMob() {
+        return MOBS[rint(MOBS.length, 0)];
+    }
+    _makeMob(mob){
+        mob = mob || this._genMob();
+        return Object.assign({}, MOBS_STATS[ mob ]);
+    }
+    _run() {
+        let action = null;
+        const statename = this.state.name;
+        this.state.ticks++;
+
+        switch(statename) {
+            case "start":
+                // battle transition
+                if(this.state.ticks === 1) {
+                    action = "The party be begins walking down the road.";
+                    break;
+                }
+                if(Math.random() > .3) {
+                    this.state = State("battle");
+
+                    const list = [...new Array(rint(2, 1))];
+                    // const mob = this._genMob();
+                    const mobs = list.map( _ => this._makeMob() );
+                    this.state.mobs = mobs;
+                    console.log('mobs', mobs);
+
+                    if(mobs === 1) action =`you see a ${mob.name}!`;
+                    else action =`you see ${mobs.map(x=>x.name).join(', ')}!`;
+                    break;
+                }
+                action = "The party travels for awhile."
+                break;
+            case "battle":
+                if(this.state.mobs.length === 0) {
+                    action = `Battle was won! Found ${rint(100,0)} gold.`;
+                    this.state = State("start");
+                    break;
+                }
+                const rnd = Math.random();
+                const mobIx = rint(this.state.mobs.length);
+                const mob = this.state.mobs[mobIx];
+
+                if(rnd > .5) {
+                    if(this.state.mobs.length === 0) {
+                        break;
+                    }
+                    
+                    const player = this.playersArr[rint(this.playersLen)];
+
+                    const playerO = this.players[player];
+                    const dm = rint(mob.dm, 1);
+                    action =`${mob.name} attacks ${player} for ${dm}!`;
+                    
+                    playerO.hp -= dm;
+                    if(playerO.hp>0) action += ` ${player} has ${playerO.hp}hp left.`;
+                    else action += ` ${player} has died!`;
+
+                    break;
+                }
+                else {
+                    action = `${mob.name} prepares an attack...`;
+                    break;
+                }
+        }
+        
+        
+        
+
+        if(action && this.onState) this.onState(action);
+    }
+}
+
+module.exports = { Game: Game };
+
+function rint(max, min) {
+    min = min || 0;
+    max = max - min;
+    return Math.floor( Math.random() * max + min);
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+const { Game } = require('./game');
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '1195063908:AAFmB45q7IivgLMC64x8nopRyk7yaJNnGsA';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 console.log('starting engine');
 // Matches "/echo [whatever]"
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-
-    console.log(msg.from.username);
+  console.log(msg.from.username);
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -24,8 +24,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 });
 
 bot.onText(/\/start (.+)/, (msg, match) => {
-
-    console.log(msg.from.username);
+  console.log(msg.from.username);
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -37,16 +36,78 @@ bot.onText(/\/start (.+)/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-bot.onText(/\/join (.+)/, (msg, match) => {
+// Join the party
+bot.onText(/\/join/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const room = rooms[chatId] = rooms[chatId] || Room();
+    rooms[chatId] = room;
 
+    if(room.game) {
+        bot.sendMessage(chatId, 'sorry, game has already started');
+        return;
+    }
+
+    const user = msg.from.username;
+    room.players[user] = Player(user);
+
+    bot.sendMessage(chatId, `${msg.from.username} thanks for joining. Type /play once everyone has joined.`);
+});
+
+function Room() {
+    return {
+        players: {},
+        game: null
+    };
+}
+function Player(name) {
+    return {
+        hp: 30,
+        dm: 3,
+        name: name
+    }
+}
+let rooms = {};
+
+const onState = (chatId, state) => {
+    if(!state) return;
+    console.log(Date.now() + ':' + chatId, state);
+    bot.sendMessage(chatId, state);
+}
+
+bot.onText(/\/play/, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  let room = rooms[chatId];
+  if(!room) {
+    // bot.sendMessage(chatId, 'must /join the party first');
+    room = rooms[chatId] = Room();
+
+    const user = msg.from.username;
+    room.players[user] = Player(user);
+    // return;   
+  }
+
+  const game = room.game = new Game(room);
+
+  
+  game.start( onState.bind(this, chatId) );
+  bot.sendMessage(chatId, `Game is starting- good luck!`);
+});
+
+bot.onText(/[\/]?(attack|kill|swing)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    let room = rooms[chatId];
+    const user = msg.from.username;
+
+    room.game.act(user, "attack");
 });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  console.log(msg.from.username, match[0]);
+  // console.log(msg.from.username, match[0]);
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  // bot.sendMessage(chatId, 'Received your message');
 });
